eaa45b59ce050b4d545637699f6cb369e9dd868d
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-06-20 21:10:20 +0900

    Feature - about java variable

---
+++ pjtTest/VarClass.java
@@ -0,0 +1,12 @@
+package pjtTest;
+
+public class VarClass {
+    public static void main(String[] args) {
+        int i = 10;
+        int j;
+        j = 100;
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("i + j = " + (i + j));
+    }
+}
