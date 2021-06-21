52215b671fe831ccd51ad60992c23b7af4061045
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-06-21 20:59:34 +0900

    Feature -  operators

--- testPjt/src/BasicDataType.java
+++ testPjt/src/BasicDataType.java
@@ -3,16 +3,25 @@ package testPjt.src;
 public class BasicDataType {
     public static void main(String[] args) {
         char c = 'a'; // char형을 초기화 할 때는 ''사용
+        int o = 012; // ==10 8진수(0으로 시작)
         int i = 10;
-        double d = 3.14;
+        int h = 0xA; // ==10 16진수(0x로 시작)
+        long l = 9223372036854775807L; // long형은 접미사 L 또는 l을 붙임(l은 1과 비슷해 보이므로 지양)
+        float f = 3.14f; // float형은 접미사 F 또는 f를 붙임
+        double d = 314e-2;
         boolean b = false;
         System.out.println("c = " + c);
-        System.out.println("i = " + i);
-        System.out.println("d = " + d);
+        System.out.println("o = " + o + ", i = " + i + ", h = " + h + ", l = " + l);
+        System.out.println("o + i + h = " + (o + i + h)); // +, -, *, /, % 등 연산 수행가능
+        System.out.println("i++ = " + (i++) + ", after i++ = " + i);
+        i = 10;
+        System.out.println("++i = " + (++i) + ", after ++i = " + i);
+        System.out.println("f = " + f + ", d = " + d);
         System.out.println("b = " + b);
 
-        String s = "Hello World!"; // String형은 객체 데이터 타입
-        System.out.println("s = " + s);
+        String s1 = "Hello World!"; // String형은 객체 데이터 타입
+        String s2 = new String("Hello World!"); // s1 == s2
+        System.out.println("s1 = " + s1 + ", s2 = " + s2);
 
         // 자동 형 변환
         byte b1 = 10;
