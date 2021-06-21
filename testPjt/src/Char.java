df37259957ecbdbdfd13d1d0a8415c0c89e6a7c6
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-06-21 21:12:17 +0900

    Feature - char data type

---
+++ testPjt/src/Char.java
@@ -0,0 +1,8 @@
+package testPjt.src;
+
+public class Char {
+    public static void main(String[] args) {
+        char a1 = 'a', a2 = 97, a3 = '\u0061'; // 문자, 아스키코드, 유니코드
+        System.out.printf("a1: %c, a2: %c, a3: %c\n", a1, a2, a3);
+    }
+}
