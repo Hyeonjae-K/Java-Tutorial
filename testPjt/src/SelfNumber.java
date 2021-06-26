da1f2f7029e47a74d49fad20d6681860d62e4bae
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-06-26 19:13:58 +0900

    Feature - self number

---
+++ testPjt/src/SelfNumber.java
@@ -0,0 +1,33 @@
+package testPjt.src;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SelfNumber {
+    public static int getSelfNum(int n) {
+        int result = n;
+        while (n != 0) {
+            result += n % 10;
+            n /= 10;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        boolean[] A = new boolean[5000];
+        Arrays.fill(A, false);
+
+        for (int i = 1; i < 5000; i++) {
+            int selfNum = getSelfNum(i);
+            if (selfNum < 5000) {
+                A[selfNum] = true;
+            }
+        }
+
+        for (int i = 1; i < 5000; i++) {
+            if (A[i] == false) {
+                System.out.println(i);
+            }
+        }
+    }
+}
