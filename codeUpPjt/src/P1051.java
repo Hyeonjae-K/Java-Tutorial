fe23141f7158e7d7cfb753e0ed2e384788713c15
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-06-28 16:05:40 +0900

    Feature - basic problems

---
+++ codeUpPjt/src/P1051.java
@@ -0,0 +1,17 @@
+package codeUpPjt.src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1051 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nums = br.readLine().split(" ");
+        if (Integer.parseInt(nums[1]) >= Integer.parseInt(nums[0])) {
+            System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
+    }
+}
